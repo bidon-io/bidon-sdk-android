04f582ef5b3f7f5e8540feb54f13de92f71343e8
Aleksei Cherniaev <klukwist@gmail.com>
2023-04-21 16:14:51 +0200

    BDN-141. Auction logic. 0.2.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,15 +1,15 @@
 object Versions {
-    const val BidonVersionName = "0.1.1-Beta"
+    const val BidonVersionName = "0.2.0"
 
     object Adapters {
-        const val Admob = "0.1.1.0-Beta"
-        const val Applovin = "0.1.1.0-Beta"
-        const val Appsflyer = "0.1.1.0-Beta"
-        const val BidMachine = "0.1.1.0-Beta"
-        const val IronSource = "0.1.1.0-Beta"
-        const val Fyber = "0.1.1.0-Beta"
-        const val DTExchange = "0.1.1.0-Beta"
-        const val UnityAds = "0.1.1.0-Beta"
+        const val Admob = "0.2.0.0"
+        const val Applovin = "0.2.0.0"
+        const val Appsflyer = "0.2.0.0"
+        const val BidMachine = "0.2.0.0"
+        const val IronSource = "0.2.0.0"
+        const val Fyber = "0.2.0.0"
+        const val DTExchange = "0.2.0.0"
+        const val UnityAds = "0.2.0.0"
     }
 }
 
