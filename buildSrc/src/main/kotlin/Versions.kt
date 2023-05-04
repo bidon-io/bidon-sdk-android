cb385c7b1bbaffab68eb342d7d572438a1b61221
Aleksei Cherniaev <klukwist@gmail.com>
2023-05-04 11:01:35 +0200

    Added cancelling auction if BannerView.destroy() is invoked

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     private val major = 0
     private val minor = 2
     private val patch = 1
-    private val semantic: String? = "-beta.6"
+    private val semantic: String? = "-beta.7"
 
     val BidonVersionName = mainVersion + semanticVersion
 
