a047c22011172afa496263c74d9103580db7944e
Aleksei Cherniaev <klukwist@gmail.com>
2023-08-01 17:49:04 +0200

    BDN-315. BigoAds. Fixed fullscreen ad params

--- adapter/bigoads/src/main/java/org/bidon/bigoads/impl/BigoBannerAuctionParams.kt
+++ adapter/bigoads/src/main/java/org/bidon/bigoads/impl/BigoBannerAuctionParams.kt
@@ -5,18 +5,18 @@ import org.bidon.sdk.ads.banner.BannerFormat
 
 data class BigoBannerAuctionParams(
     val bannerFormat: BannerFormat,
-    val payload: String,
     val slotId: String,
     val bidPrice: Double,
+    val payload: String,
 ) : AdAuctionParams {
     override val adUnitId: String get() = slotId
     override val pricefloor: Double get() = bidPrice
 }
 
 data class BigoFullscreenAuctionParams(
-    val payload: String,
     val slotId: String,
     val bidPrice: Double,
+    val payload: String,
 ) : AdAuctionParams {
     override val adUnitId: String get() = slotId
     override val pricefloor: Double get() = bidPrice
