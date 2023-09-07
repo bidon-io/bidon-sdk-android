3cb537029df2da28fb4c23c12393ca7d79ed375d
Aleksei Cherniaev <klukwist@gmail.com>
2023-09-07 15:47:51 +0200

    BDN-191. Positioned Banner. Check banner size on show

--- bidon/src/main/java/org/bidon/sdk/ads/banner/render/AdRendererImpl.kt
+++ bidon/src/main/java/org/bidon/sdk/ads/banner/render/AdRendererImpl.kt
@@ -99,10 +99,10 @@ internal class AdRendererImpl(
         if (positionState !is PositionState.Place) return true
         return when (positionState.position) {
             BannerPosition.HorizontalTop,
-            BannerPosition.HorizontalBottom -> screenSize.x > this.obtainWidth()
+            BannerPosition.HorizontalBottom -> screenSize.x >= this.obtainWidth()
 
             BannerPosition.VerticalLeft,
-            BannerPosition.VerticalRight -> screenSize.y > this.obtainWidth()
+            BannerPosition.VerticalRight -> screenSize.y >= this.obtainWidth()
         }
     }
 
