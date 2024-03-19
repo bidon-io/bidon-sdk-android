f5fb8848b0ac762d4748b564376fc9dd0bc930c9
anton_zarytski <antonzaritsky@gmail.com>
2024-03-19 14:54:31 +0100

    Feature/BDN-616: reverted semantic version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     private val major = 0
     private val minor = 4
     private val patch = 29
-    private val semantic: String = "1"//""-alpha.2"
+    private val semantic: String = ""//""-alpha.2"
 
     val BidonVersionName = mainVersion + semanticVersion
 
