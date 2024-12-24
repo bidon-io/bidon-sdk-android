06794006ea468d4960ed7c170e806f54d745a9da
Glavatskikh Denis <denis.glavatskikh@appodeal.com>
2024-12-24 15:09:34 +0100

    release/0.8.0-next.2: updated CHANGELOG.md; bump sdk version;

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@ object Versions {
     private val minor = 8
     private val patch = 0
 
-    private val semantic: String = "-next.1"
+    private val semantic: String = "-next.2"
 
     val BidonVersionName = mainVersion + semanticVersion
 
