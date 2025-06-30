4d4c8a329026be4e26ce397e9725fd82adf7d429
anton_zarytski <antonzaritsky@gmail.com>
2025-06-30 13:06:16 +0200

    Feature/BDN-958: Self review

--- bidon/src/main/java/org/bidon/sdk/databinders/user/impl/appset/AppSetRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.bidon.sdk.databinders.user.impl.appset
-
-import android.content.Context
-
-internal interface AppSetRepository {
-    suspend fun getAppSetId(context: Context): String?
-    suspend fun getAppSetIdScope(context: Context): String?
-}
