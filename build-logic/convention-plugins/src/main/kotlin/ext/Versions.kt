dee972395b2aa64157f6e883c671e17bc5bbe1e4
Glavatskikh Denis <denis.glavatskikh@appodeal.com>
2025-08-20 13:49:15 +0200

    Revert "release/0.10.0: tried alpha.3 for ci/cd;"
    This reverts commit 3b7a17d9493f344692446f704dd7445b4cf5f259.

--- build-logic/convention-plugins/src/main/kotlin/ext/Versions.kt
+++ build-logic/convention-plugins/src/main/kotlin/ext/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     private val major = 0
     private val minor = 10
     private val patch = 0
-    private val semantic: String = "-alpha.3"
+    private val semantic: String = "-alpha.1"
 
     val BidonVersionName = mainVersion + semanticVersion
 
