6a66dbd804a7294cccc404367cc7a0ab03484fcc
Vladimir <vladkhrolov@gmail.com>
2026-02-16 16:14:31 +0100

    Feature/BDN-1103 Move Zmaticoo GetToken logic to use case

--- adapter/zmaticoo/src/main/java/org/bidon/zmaticoo/ZmaticooAdapter.kt
+++ adapter/zmaticoo/src/main/java/org/bidon/zmaticoo/ZmaticooAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import com.maticoo.sdk.InitConfiguration
 import com.maticoo.sdk.core.InitCallback
 import com.maticoo.sdk.core.MaticooAds
-import com.zmaticoo.sdk.base.common.ZMaticooAds
 import com.zmaticoo.sdk.flow.model.ComponentError
 import org.bidon.sdk.adapter.AdProvider
 import org.bidon.sdk.adapter.AdSource
@@ -13,14 +12,13 @@ import org.bidon.sdk.adapter.AdapterInfo
 import org.bidon.sdk.adapter.DemandId
 import org.bidon.sdk.adapter.Initializable
 import org.bidon.sdk.adapter.SupportsRegulation
-import org.bidon.sdk.ads.banner.BannerFormat
 import org.bidon.sdk.auction.AdTypeParam
 import org.bidon.sdk.logs.logging.impl.logError
-import org.bidon.sdk.logs.logging.impl.logInfo
 import org.bidon.sdk.regulation.Regulation
 import org.bidon.zmaticoo.ext.adapterVersion
 import org.bidon.zmaticoo.ext.asBidonError
 import org.bidon.zmaticoo.ext.sdkVersion
+import org.bidon.zmaticoo.impl.GetTokenUseCase
 import org.bidon.zmaticoo.impl.ZmaticooBannerAuctionParams
 import org.bidon.zmaticoo.impl.ZmaticooBannerImpl
 import org.bidon.zmaticoo.impl.ZmaticooFullscreenAuctionParams
@@ -56,36 +54,7 @@ internal class ZmaticooAdapter :
     private var placements: List<PlacementConfig> = emptyList()
 
     override suspend fun getToken(adTypeParam: AdTypeParam): String? {
-        val tokensMap = mutableMapOf<String, JSONObject>()
-
-        val targetFormat = when (adTypeParam) {
-            is AdTypeParam.Banner -> if (adTypeParam.bannerFormat == BannerFormat.MRec) {
-                PlacementFormat.MREC
-            } else {
-                PlacementFormat.BANNER
-            }
-            is AdTypeParam.Interstitial -> PlacementFormat.INTERSTITIAL
-            is AdTypeParam.Rewarded -> PlacementFormat.REWARDED
-        }
-
-        for (placement in placements.filter { it.format == targetFormat }) {
-            val timestamp = System.currentTimeMillis()
-            val token = ZMaticooAds.getBiddingToken(placement.placementId, timestamp)
-            if (!token.isNullOrEmpty()) {
-                tokensMap[placement.placementId] =
-                    JSONObject().apply {
-                        put("token", token)
-                        put("timestamp", timestamp)
-                    }
-            }
-        }
-
-        return if (tokensMap.isNotEmpty()) {
-            logInfo(TAG, "getToken: tokensMap $tokensMap")
-            JSONObject(tokensMap as Map<*, *>).toString()
-        } else {
-            null
-        }
+        return GetTokenUseCase(adTypeParam, placements)
     }
 
     override suspend fun init(
---
+++ adapter/zmaticoo/src/main/java/org/bidon/zmaticoo/impl/GetTokenUseCase.kt
@@ -0,0 +1,52 @@
+package org.bidon.zmaticoo.impl
+
+import com.zmaticoo.sdk.base.common.ZMaticooAds
+import org.bidon.sdk.ads.banner.BannerFormat
+import org.bidon.sdk.auction.AdTypeParam
+import org.bidon.sdk.logs.logging.impl.logInfo
+import org.bidon.zmaticoo.PlacementConfig
+import org.bidon.zmaticoo.PlacementFormat
+import org.json.JSONObject
+
+/**
+ * Created by Vladimir Khrolovich on 16/02/2026.
+ */
+internal object GetTokenUseCase {
+    operator fun invoke(
+        adTypeParam: AdTypeParam,
+        placements: List<PlacementConfig>
+    ): String? {
+        val tokensMap = mutableMapOf<String, JSONObject>()
+
+        val targetFormat = when (adTypeParam) {
+            is AdTypeParam.Banner -> if (adTypeParam.bannerFormat == BannerFormat.MRec) {
+                PlacementFormat.MREC
+            } else {
+                PlacementFormat.BANNER
+            }
+            is AdTypeParam.Interstitial -> PlacementFormat.INTERSTITIAL
+            is AdTypeParam.Rewarded -> PlacementFormat.REWARDED
+        }
+
+        for (placement in placements.filter { it.format == targetFormat }) {
+            val timestamp = System.currentTimeMillis()
+            val token = ZMaticooAds.getBiddingToken(placement.placementId, timestamp)
+            if (!token.isNullOrEmpty()) {
+                tokensMap[placement.placementId] =
+                    JSONObject().apply {
+                        put("token", token)
+                        put("timestamp", timestamp)
+                    }
+            }
+        }
+
+        return if (tokensMap.isNotEmpty()) {
+            logInfo(TAG, "getToken: tokensMap $tokensMap")
+            JSONObject(tokensMap as Map<*, *>).toString()
+        } else {
+            null
+        }
+    }
+}
+
+private const val TAG = "ZmaticooGetTokenUseCase"
