be0d3e33aa5675ac6111e1246fa681282d32c94c
Glavatskikh Denis <denis.glavatskikh@appodeal.com>
2026-03-06 17:52:58 +0100

    feat(cache): bump version to 0.13.0-ac.4;

--- build-logic/convention-plugins/src/main/kotlin/ext/Versions.kt
+++ build-logic/convention-plugins/src/main/kotlin/ext/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     private const val major = 0
     private const val minor = 13
     private const val patch = 0
-    private const val semantic: String = "-ac.3"
+    private const val semantic: String = "-ac.4"
 
     val BidonVersionName = mainVersion + semanticVersion
 
