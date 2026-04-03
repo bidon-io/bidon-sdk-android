80e158227d27f80139fb8035979b292ed21b26eb
Glavatskikh Denis <denis.glavatskikh@appodeal.com>
2026-04-03 16:15:04 +0200

    chore: bump version to 0.13.0-ac.6

--- build-logic/convention-plugins/src/main/kotlin/ext/Versions.kt
+++ build-logic/convention-plugins/src/main/kotlin/ext/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     private const val major = 0
     private const val minor = 13
     private const val patch = 0
-    private const val semantic: String = "-ac.5"
+    private const val semantic: String = "-ac.6"
 
     val BidonVersionName = mainVersion + semanticVersion
 
