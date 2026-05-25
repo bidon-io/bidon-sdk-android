6848d4616160c566ec9f5dba130908d8c202f250
Alex Cherniaev <klukwist@gmail.com>
2026-05-25 22:13:40 +0200

    feat(JUNI-3): add gma/ext/Ext.kt

---
+++ adapter/gma/src/main/java/org/bidon/gma/ext/Ext.kt
@@ -0,0 +1,34 @@
+package org.bidon.gma.ext
+
+import com.google.android.gms.ads.AdFormat
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.MobileAds
+import org.bidon.gma.BuildConfig
+import org.bidon.sdk.ads.banner.BannerFormat
+import org.bidon.sdk.ads.banner.helper.DeviceInfo.isTablet
+import org.bidon.sdk.auction.AdTypeParam
+
+internal var adapterVersion = BuildConfig.ADAPTER_VERSION
+internal var sdkVersion = MobileAds.getVersion().toString()
+
+internal fun BannerFormat.toGmaAdSize(): AdSize {
+    return when (this) {
+        BannerFormat.Banner -> AdSize.BANNER
+        BannerFormat.LeaderBoard -> AdSize.LEADERBOARD
+        BannerFormat.MRec -> AdSize.MEDIUM_RECTANGLE
+        BannerFormat.Adaptive -> {
+            if (isTablet) {
+                AdSize.LEADERBOARD
+            } else {
+                AdSize.BANNER
+            }
+        }
+    }
+}
+
+internal fun AdTypeParam.getAdFormat() =
+    when (this) {
+        is AdTypeParam.Banner -> AdFormat.BANNER
+        is AdTypeParam.Interstitial -> AdFormat.INTERSTITIAL
+        is AdTypeParam.Rewarded -> AdFormat.REWARDED
+    }
