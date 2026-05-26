8186ff4df36d780715439cec2eb1675ca4198294
Alex Cherniaev <klukwist@gmail.com>
2026-05-26 11:51:46 +0200

    feat(gma): add GmaAdapter main class

---
+++ adapter/gma/src/main/java/org/bidon/gma/GmaAdapter.kt
@@ -0,0 +1,67 @@
+package org.bidon.gma
+
+import android.content.Context
+import com.google.android.gms.ads.MobileAds
+import org.bidon.gma.ext.adapterVersion
+import org.bidon.gma.ext.sdkVersion
+import org.bidon.gma.impl.GmaBannerImpl
+import org.bidon.gma.impl.GmaInterstitialImpl
+import org.bidon.gma.impl.GmaRewardedImpl
+import org.bidon.sdk.adapter.AdProvider
+import org.bidon.sdk.adapter.AdSource
+import org.bidon.sdk.adapter.Adapter
+import org.bidon.sdk.adapter.AdapterInfo
+import org.bidon.sdk.adapter.DemandId
+import org.bidon.sdk.adapter.Initializable
+import org.json.JSONObject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+internal val GmaDemandId = DemandId("gma")
+
+/**
+ * Google Mobile Ads (GMA) adapter.
+ * Uses plain AdRequest.Builder().build() — no AdMobAdapter mediation bundle.
+ * Don't forget to disable automatic refresh for each AdUnit in the GMA console.
+ */
+@Suppress("unused")
+internal class GmaAdapter :
+    Adapter.Network,
+    Initializable<GmaInitParameters>,
+    AdProvider.Banner<GmaBannerAuctionParams>,
+    AdProvider.Rewarded<GmaFullscreenAdAuctionParams>,
+    AdProvider.Interstitial<GmaFullscreenAdAuctionParams> {
+
+    override val demandId = GmaDemandId
+    override val adapterInfo = AdapterInfo(
+        adapterVersion = adapterVersion,
+        sdkVersion = sdkVersion
+    )
+
+    override suspend fun init(context: Context, configParams: GmaInitParameters): Unit =
+        suspendCoroutine { continuation ->
+            MobileAds.initialize(context) {
+                continuation.resume(Unit)
+            }
+        }
+
+    override fun interstitial(): AdSource.Interstitial<GmaFullscreenAdAuctionParams> {
+        return GmaInterstitialImpl()
+    }
+
+    override fun rewarded(): AdSource.Rewarded<GmaFullscreenAdAuctionParams> {
+        return GmaRewardedImpl()
+    }
+
+    override fun banner(): AdSource.Banner<GmaBannerAuctionParams> {
+        return GmaBannerImpl()
+    }
+
+    override fun parseConfigParam(json: String): GmaInitParameters {
+        val jsonObject = JSONObject(json)
+        return GmaInitParameters(
+            requestAgent = jsonObject.optString("request_agent"),
+            queryInfoType = jsonObject.optString("query_info_type")
+        )
+    }
+}
