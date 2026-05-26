41a8df2d5de8e1983952a6069b6b35f9f8b434df
Alex Cherniaev <klukwist@gmail.com>
2026-05-26 11:50:05 +0200

    feat(gma): add GmaInitParameters.kt with auction param sealed interfaces

---
+++ adapter/gma/src/main/java/org/bidon/gma/GmaInitParameters.kt
@@ -0,0 +1,51 @@
+package org.bidon.gma
+
+import android.app.Activity
+import com.google.android.gms.ads.AdSize
+import org.bidon.gma.ext.toGmaAdSize
+import org.bidon.sdk.adapter.AdAuctionParams
+import org.bidon.sdk.adapter.AdapterParameters
+import org.bidon.sdk.ads.banner.BannerFormat
+import org.bidon.sdk.auction.models.AdUnit
+
+internal class GmaInitParameters(
+    val requestAgent: String?,
+    val queryInfoType: String?
+) : AdapterParameters
+
+internal sealed interface GmaBannerAuctionParams : AdAuctionParams {
+    val activity: Activity
+    val bannerFormat: BannerFormat
+    val containerWidth: Float
+    val adSize: AdSize get() = bannerFormat.toGmaAdSize()
+
+    class Network(
+        override val activity: Activity,
+        override val bannerFormat: BannerFormat,
+        override val containerWidth: Float,
+        override val adUnit: AdUnit,
+    ) : GmaBannerAuctionParams {
+        override val price: Double = adUnit.pricefloor
+        val adUnitId: String? = adUnit.extra?.getString("ad_unit_id")
+
+        override fun toString(): String {
+            return "GmaBannerAuctionParams($adUnit)"
+        }
+    }
+}
+
+internal sealed interface GmaFullscreenAdAuctionParams : AdAuctionParams {
+    val activity: Activity
+
+    class Network(
+        override val activity: Activity,
+        override val adUnit: AdUnit,
+    ) : GmaFullscreenAdAuctionParams {
+        override val price: Double = adUnit.pricefloor
+        val adUnitId: String? = adUnit.extra?.getString("ad_unit_id")
+
+        override fun toString(): String {
+            return "GmaFullscreenAdAuctionParams($adUnit)"
+        }
+    }
+}
